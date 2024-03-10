63a2fa5ced3458fd914c22d17a7ea03b541ea79c
Peter Baker <pcbaker1969@gmail.com>
2024-03-10 18:26:39 -0400

    recommendation system start

--- agent.py
+++ agent.py
@@ -1,3 +1,110 @@
+import random
+
+
 class Agent:
-    def __init__(self, name):
+    def __init__(self, name, interactions, similar_words):
         self.name = name
+        self.interactions = interactions
+        self.similar_words = similar_words
+
+    def get_name(self):
+        return self.name
+
+    # G.add_nodes_from([('person1', {'name': 'John Doe', 'age': 40})])
+    def get_node_for_graph(self):
+        node = {'name': self.name,
+                'interactions': self.interactions,
+                'similar_words': self.similar_words}
+        print("node = ", node)
+        return node
+
+    # This is for parent node, find the match based on similar words
+    def calc_compare_type(self, other):
+        return abs(self.similar_words - other.similar_words)
+
+    # this is to find the most popular video
+    def get_interactions(self):
+        return self.interactions
+
+
+class CC_Model:
+
+    def __init__(self):
+        self.count = 0
+        self.interactions_avg = 0
+        self.nodes = []
+
+    def update_interactions_avg(self):
+        val = 0
+        for n in self.nodes:
+            val += n.get_interactions()
+        self.interactions_avg = val/len(self.nodes)
+        print("average interaction = ", self.interactions_avg)
+
+    def get_closest_match(self, node, exclude_nodes=()):
+        # do comparison based on "node", but don't pick yourself
+        tmp_lst = self.nodes[:]
+        tmp_lst.remove(node)
+        for exc in exclude_nodes:
+            tmp_lst.remove(exc)
+        best_node = None
+        best_score = 10
+        for n in tmp_lst:
+            score = n.calc_compare_type(node)
+            if score < best_score:
+                best_score = score
+                best_node = n
+        return best_node
+
+    def get_most_popular_match(self, node, exclude_nodes=()):
+        tmp_lst = self.nodes[:]
+        tmp_lst.remove(node)
+        for exc in exclude_nodes:
+            tmp_lst.remove(exc)
+        best_node = None
+        best_score = 10
+        for n in tmp_lst:
+            if n.get_interactions() > self.interactions_avg:
+                score = n.calc_compare_type(node)
+                if score < best_score:
+                    best_score = score
+                    best_node = n
+        return best_node
+
+    # when adding an edge, one node will be picked at random
+    def get_rnd_node(self):
+        node = random.choice(self.nodes)
+        return node
+
+    def get_count(self):
+        return self.count
+
+    def create_rnd_node(self):
+        self.count += 1
+        name = self.count
+        interactions = 0
+        similar_words = random.randint(0, 10)
+        node = Agent(name, interactions, similar_words)
+        self.nodes.append(node)
+        self.update_interactions_avg()
+        return node
+
+    # starter nodes have interaction scores
+    def create_starter_node(self):
+        self.count += 1
+        name = self.count
+        interactions = 0
+        similar_words = random.randint(0, 10)
+        # this way we know n1 interactions > n2 interactions etc for initial
+        # edge selections (3 nodes and 2 edges)
+        interactions = 5 - self.count
+        node = Agent(name, interactions, similar_words)
+        self.nodes.append(node)
+        self.update_interactions_avg()
+        return node
+
+    def get_node_name_lst(self):
+        lst = []
+        for n in self.nodes:
+            lst.append(n.get_name())
+        return lst
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # 1. Objectives
-# Primary Objective: To model and analyze how clustering coefficients
-# evolve in a social network and how friends-of-friends connections
+# Primary Objective: To model and analyze how clustering
+# evolves in a social network and how connections
 # contribute to the formation of tight-knit communities over time.
 # Secondary Objectives: To identify key factors influencing high
 # clustering coefficients and to simulate various scenarios to understand
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
+import agent as ag
 
 import random
 import sys
@@ -19,6 +20,7 @@ btnWt = 20
 lblWt = 10
 compFrmHt = 100
 kVal = 25
+new_edges_per_new_node = 3
 
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg
@@ -90,6 +92,8 @@ tran_lbl.pack(side="left")
 tran_val_lbl = tk.Label(infoFrm, text="0", width=lblWt)
 tran_val_lbl.pack(side="left")
 
+cc_model = ag.CC_Model()
+
 g = nx.DiGraph()
 
 
@@ -101,11 +105,11 @@ def draw_graph():
                      with_labels=True, node_size=100, node_color="green")
     if len(g.edges) > 3:
         tra_cnt = nx.transitivity(g)
-        print("tri_cnt = ", tra_cnt)
+        #print("tri_cnt = ", tra_cnt)
         if tra_cnt is not None:
             tran_val_lbl.config(text="{:.4f}".format(tra_cnt))
             clus = nx.clustering(g)
-            print("clustering = ", clus)
+            #print("clustering = ", clus)
             listbox.delete(0, END)
             keys = list(clus.keys())
             cnt = 1
@@ -123,14 +127,17 @@ def reset_canvas():
     fig.set_facecolor("black")
 
 
-mlist = range(2, 99)
-mlist_idx = 0
-
-
 def setup():
     reset_canvas()
     g.clear()
-    g.add_edge(1, mlist[1])
+    n1 = cc_model.create_starter_node()
+    n2 = cc_model.create_starter_node()
+    n3 = cc_model.create_starter_node()
+    g.add_nodes_from([(n1.get_name(), n1.get_node_for_graph()),
+                      (n2.get_name(), n2.get_node_for_graph()),
+                      (n3.get_name(), n3.get_node_for_graph())])
+    g.add_edge(n1.get_name(), n2.get_name())
+    g.add_edge(n1.get_name(), n3.get_name())
     plt.gca().set_facecolor("grey")
     fig.set_facecolor("black")
     draw_graph()
@@ -142,24 +149,39 @@ canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=True)
 
 def add_node_to_graph():
     reset_canvas()
-    # get node from graph and randomly select
-    # eventually make less random by attributes
-    nlist = list(g.nodes)
-    g.add_edge(random.choice(nlist), random.choice(mlist))
-
+    n_new = cc_model.create_rnd_node()
+    # nrp will be the parent that picks you based on similar words
+    nrp = cc_model.get_closest_match(n_new)
+    # nr2 and nr3 will be recs that your video provides
+    # nr2 will be above average interactions and similar words
+    nr2 = cc_model.get_most_popular_match(n_new, [nrp])
+    # nr3 will be only based on similar words
+    nr3 = cc_model.get_closest_match(n_new, [nrp, nr2])
+    g.add_nodes_from([(n_new.get_name(), n_new.get_node_for_graph())])
+    g.add_edge(nrp.get_name(), n_new.get_name())
+    print("parent to new: {} -> {}".format(nrp.get_name(), n_new.get_name()))
+    g.add_edge(n_new.get_name(), nr2.get_name())
+    print("new to rec1: {} -> {}".format(n_new.get_name(), nr2.get_name()))
+    g.add_edge(n_new.get_name(), nr3.get_name())
+    print("new to rec2: {} -> {}".format(n_new.get_name(), nr3.get_name()))
+    lst = cc_model.get_node_name_lst()
+    print("list of nodes: ", lst)
+    dlst = g.in_degree(lst)
+    print("degrees: ", list(dlst))
     draw_graph()
 
 
 def add_edge_to_graph():
     reset_canvas()
     # get node from graph and randomly select
+    n2 = cc_model.get_rnd_node()
     # eventually make less random by attributes
-    n1, n2 = g_utils.pick_2_unique_nodes(g)
-    if n1 is None or n2 is None:
-        print("did not create an edge")
+    n1 = cc_model.get_rec_node(n2)
+    if n1 is None:
+        print("Can't add node in add_edge_to_graph")
         return
-    print("Adding edge ({}, {})".format(n1, n2))
-    g.add_edge(n1, n2)
+    print("Adding edge ({}, {})".format(n1.get_name(), n2.get_name()))
+    g.add_edge(n1.get_name(), n2.get_name())
     draw_graph()
 
 
