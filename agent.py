0e15323c0ca478105568b63dcbaaf5cbe91ec9dc
Peter Baker <pcbaker1969@gmail.com>
2024-03-16 14:57:21 -0400

    node to edge works, but distribution is off

--- agent.py
+++ agent.py
@@ -13,7 +13,6 @@ class Agent:
         if tick is not None:
             self.refresh_date = tick
 
-
     def get_name(self):
         return self.name
 
@@ -26,7 +25,6 @@ class Agent:
         node = {'name': self.name,
                 'interactions': self.interactions,
                 'similar_words': self.similar_words}
-        print("node = ", node)
         return node
 
     # This is for parent node, find the match based on similar words
@@ -43,82 +41,151 @@ class Agent:
 
 class CC_Model:
 
-    def __init__(self, refresh_threshold=3, min_interactions=2):
+    def __init__(self, refresh_threshold=3, min_interactions=2, score_rng=10):
         self.count = 0
         # used in get most popular node, when creating a node, the new
         # node's edge points to one node that is above average and similar
         # 2nd edge points to most similar
         self.interactions_avg = 0
+        # Current highest interactions for a node
+        self.interactions_max = 0
         # the node can sit this many ticks before it can be stale
         self.refresh_threshold = refresh_threshold
         # the node must have at least this many interactions before being stale
         self.min_interactions = min_interactions
         self.nodes = []
+        # score range is for similarity range, 0-10 is default
+        self.score_rng = score_rng
+        self.removals = 0
+        self.skips = 0
+        self.worse_edges = 0
+        self.worse_skips = 0
 
     def reset(self):
         self.count = 0
         self.interactions_avg = 0
         self.nodes = []
 
+    def get_node_by_name(self, name):
+        for n in self.nodes:
+            if n.get_name() == name:
+                return n
+
     def get_count(self):
         return self.count
+
+    def get_nodes(self):
+        return self.nodes
+
+    def get_worse_match_nodes(self, edges):
+        # 0 is the best match, no difference in words
+        worse_match_node_val = 0
+        avg = 0
+        u = None
+        v = None
+        score = self.score_rng
+        for e in edges:
+            n = self.get_node_by_name(e[0])
+            m = self.get_node_by_name(e[1])
+            score = n.calc_compare_type(m)
+            avg += score
+            if worse_match_node_val >= score:
+                u = m
+                v = n
+                worse_match_node_val = score
+        #print("avg = ", avg/len(edges))
+        if u is not None and v is not None:
+            self.worse_edges += 1
+            #print("worse match is u={} and v={} score = {}".format(u.get_name(), v.get_name(), score))
+        else:
+            self.worse_skips += 1
+            #print("no worst matches found")
+        return u, v, worse_match_node_val
 
     def get_stale_node(self):
         stalest_node_val = self.count
         stalest_node = None
         for n in self.nodes:
-            if self.interactions_avg >= n.get_interactions() >= self.min_interactions:
-                if stalest_node_val >= n.get_refresh_date() + self.refresh_threshold:
+            if self.interactions_avg >= n.get_interactions():
+                if stalest_node_val <= n.get_refresh_date():
                     stalest_node_val = n.get_refresh_date()
                     stalest_node = n
         if stalest_node is not None:
-            print("stale: {}, date: {}, inter: {} "
-                  .format(stalest_node.get_name(),
-                          stalest_node.get_refresh_date(),
-                          stalest_node.get_interactions()))
+            self.removals += 1
         else:
-            print("no stale nodes found")
+            self.skips += 1
         return stalest_node
 
     def update_interactions_avg(self):
         val = 0
+        refresh_val = 0
+        max_interaction = 0
         for n in self.nodes:
+            if n.get_interactions() > max_interaction:
+                max_interaction = n.get_interactions()
             val += n.get_interactions()
+            refresh_val += n.get_refresh_date()
         self.interactions_avg = val / len(self.nodes)
-        print("average interaction = ", self.interactions_avg)
+        self.refresh_threshold = refresh_val / len(self.nodes)
+        self.interactions_max = max_interaction
 
-    def get_closest_match(self, node, exclude_nodes=()):
+    def get_parent_match(self, node):
         # do comparison based on "node", but don't pick yourself
         tmp_lst = self.nodes[:]
         tmp_lst.remove(node)
-        for exc in exclude_nodes:
-            try:
-                tmp_lst.remove(exc)
-            except ValueError:
-                print("tried to remove something not in list")
         best_node = None
-        best_score = 10
+        best_score = self.interactions_max
         for n in tmp_lst:
             score = n.calc_compare_type(node)
             if score <= best_score:
                 best_score = score
                 best_node = n
+        if best_node is None:
+            best_node = random.choice(tmp_lst)
         return best_node
 
-    def get_most_popular_match(self, node, exclude_nodes=()):
+    def get_similar_match(self, node, node_parent, num=2):
+        # do comparison based on "node", but don't pick yourself
         tmp_lst = self.nodes[:]
+        # don't pick yourself of your parent
         tmp_lst.remove(node)
-        for exc in exclude_nodes:
-            tmp_lst.remove(exc)
-        best_node = None
-        best_score = 10
-        for n in tmp_lst:
-            if n.get_interactions() >= self.interactions_avg:
+        tmp_lst.remove(node_parent)
+
+        best_arr = []
+        for i in range(0, num):
+            best_node = None
+            best_score = self.score_rng
+            for n in tmp_lst:
                 score = n.calc_compare_type(node)
-                if score < best_score:
+                if score <= best_score:
                     best_score = score
                     best_node = n
-        return best_node
+            if best_node is not None:
+                best_arr.append(best_node)
+                tmp_lst.remove(best_node)
+        return best_arr
+
+    def get_popular_matches(self, node, node_parent=None, num=1):
+        tmp_lst = self.nodes[:]
+        # don't pick yourself of your parent
+        tmp_lst.remove(node)
+        if node_parent is not None:
+            tmp_lst.remove(node_parent)
+        best_arr = []
+        for i in range(0, num):
+            best_node = None
+            best_score = self.score_rng
+            for n in tmp_lst:
+                if n.get_interactions() >= self.interactions_avg:
+                    score = n.calc_compare_type(node)
+                    if score < best_score:
+                        best_score = score
+                        best_node = n
+            if best_node is not None:
+                best_arr.append(best_node)
+                tmp_lst.remove(best_node)
+
+        return best_arr
 
     # when adding an edge, one node will be picked at random
     def get_rnd_node(self):
@@ -129,24 +196,13 @@ class CC_Model:
         self.count += 1
         name = self.count
         interactions = 0
-        similar_words = random.randint(0, 10)
+        similar_words = random.randint(0, self.score_rng)
         node = Agent(name, interactions, similar_words, self.count)
         self.nodes.append(node)
         self.update_interactions_avg()
         return node
 
     # starter nodes have interaction scores
-    def create_starter_node(self):
-        self.count += 1
-        name = self.count
-        similar_words = random.randint(0, 10)
-        # this way we know n1 interactions > n2 interactions etc for initial
-        # edge selections (3 nodes and 2 edges)
-        interactions = 0
-        node = Agent(name, interactions, similar_words, self.count)
-        self.nodes.append(node)
-        self.update_interactions_avg()
-        return node
 
     def get_node_name_lst(self):
         lst = []
@@ -159,3 +215,7 @@ class CC_Model:
         for n in self.nodes:
             lst.append(n.get_name_and_scores())
         return lst
+
+    def get_skips_and_removal_num(self):
+        return ("skips={}, removals={}, worse edge skips={}, worse edge removes={}"
+                .format(self.skips, self.removals, self.worse_skips, self.worse_edges))
--- main.py
+++ main.py
@@ -1,226 +1,104 @@
 # 1. Objectives
-# Primary Objective: To model and analyze how clustering
-# evolves in a social network and how connections
-# contribute to the formation of tight-knit communities over time.
-# Secondary Objectives: To identify key factors influencing high
-# clustering coefficients and to simulate various scenarios to understand
-# their impacts on social network dynamics.
+# Primary Objective: To model a social network recommendation
+# system similar to YouTube where network structure is a hybrid
+# scale-free network
 import networkx as nx
-import matplotlib.pyplot as plt
-import tkinter as tk
-from tkinter import *
-from tkinter import messagebox
-from tkinter import ttk
 import agent as ag
-import ui
-from matplotlib.backends.backend_tkagg import (
-    FigureCanvasTkAgg
-)
 import random as rnd
-btnWt = 20
-lblWt = 10
-compFrmHt = 100
-kVal = 25
-new_edges_per_new_node = 3
-# change somethign
 
-def on_closing():
-    if messagebox.askokcancel("Quit", "Do you want to quit?"):
-        root.quit()
+start_node_num = 4
+tot_nodes = 200
+
+log_list = []
+nodes_per_step = 4
 
 
 def export_graph():
     edges = g.edges
-    f_ref = open("output.csv", "w")
+    f_ref = open(f"output_{tot_nodes}_3.csv", "w")
     f_ref.write("Source, Target, Link\n")
     for e in edges:
         f_ref.write("{}, {}, {}\n".format(e[0], e[1], "1"))
     f_ref.close()
 
 
-root = Tk()
-root.title("cc agents")
-root.protocol('WM_DELETE_WINDOW', on_closing)
-
-# Creating Menubar
-menubar = Menu(root)
-
-# Adding File Menu and commands
-file = Menu(menubar, tearoff=0)
-menubar.add_cascade(label='File', menu=file)
-file.add_command(label='Export Graph', command=export_graph)
-file.add_command(label='Export Agents', command=None)
-
-# Adding Help Menu
-help_ = Menu(menubar, tearoff=0)
-menubar.add_cascade(label='Help', menu=help_)
-help_.add_command(label='CC Agents', command=None)
-help_.add_separator()
-
-# display Menu
-root.config(menu=menubar)
-
-fig = plt.figure(frameon=False, figsize=(5, 3), dpi=100)
-canvas = FigureCanvasTkAgg(fig, root)
-
-tabControl = ttk.Notebook(root)
-clusterTab = ttk.Frame(tabControl)
-degreeTab = ttk.Frame(tabControl)
-scoresTab = ttk.Frame(tabControl)
-
-tabControl.add(clusterTab, text="Clustering")
-tabControl.add(degreeTab, text="Degree")
-tabControl.add(scoresTab, text="Scores")
-tabControl.pack(side="right", padx=10, expand=1, fill="both")
-
-componentFrm = tk.Frame(root, height=compFrmHt)
-componentFrm.pack(padx=10, pady=10, side="bottom")
-
-# ----------------- Notebook (tab1) --------------------
-cluster_listbox = ui.make_lst_box(clusterTab, "Clustering: Node, C.C.")
-degree_listbox = ui.make_lst_box(degreeTab, "Degree: Node, In-Degree")
-score_listbox = ui.make_lst_box(scoresTab, "Scores: Node, Int, Wrds, Tck")
-
-# ----------------- Button Frame -------------------
-btnFrm = tk.Frame(componentFrm)
-btnFrm.pack(padx=10, pady=10, side="right")
-setupBtn = tk.Button(btnFrm, text="Setup", width=btnWt,
-                     command=lambda: setup())
-setupBtn.pack(side="top")
-addNodeBtn = tk.Button(btnFrm, text="Step", width=btnWt,
-                       command=lambda: add_node_to_graph())
-addNodeBtn.pack(side="top")
-
-# ----------------- Info Frame -------------------
-infoFrm = tk.Frame(componentFrm)
-infoFrm.pack(padx=10, pady=10, side="left")
-node_lbl = tk.Label(infoFrm, text="Nodes:", width=lblWt)
-node_lbl.pack(side="left")
-node_val_lbl = tk.Label(infoFrm, text="2", width=lblWt)
-node_val_lbl.pack(side="left")
-
-edge_lbl = tk.Label(infoFrm, text="Edges:", width=lblWt)
-edge_lbl.pack(side="left")
-edge_val_lbl = tk.Label(infoFrm, text="1", width=lblWt)
-edge_val_lbl.pack(side="left")
-
-tran_lbl = tk.Label(infoFrm, text="Transitivity:", width=lblWt)
-tran_lbl.pack(side="left")
-
-tran_val_lbl = tk.Label(infoFrm, text="0", width=lblWt)
-tran_val_lbl.pack(side="left")
-
+g = nx.DiGraph()
 cc_model = ag.CC_Model()
 
-g = nx.DiGraph()
-
-
-def draw_graph():
-    node_val_lbl.config(text=f"{len(g.nodes)}")
-    edge_val_lbl.config(text=f"{len(g.edges)}")
-
-    nx.draw_networkx(g, pos=nx.spring_layout(g, kVal), alpha=1,
-                     with_labels=True, node_size=100, node_color="green")
-    if len(g.edges) > 3:
-        tra_cnt = nx.transitivity(g)
-
-        if tra_cnt is not None:
-            tran_val_lbl.config(text="{:.4f}".format(tra_cnt))
-            clus = nx.clustering(g)
-            cluster_listbox.delete(0, END)
-            keys = list(clus.keys())
-            cnt = 1
-            for k in keys:
-                s = "N: {}, C.C. {:.4f}".format(k, clus[k])
-                cluster_listbox.insert(cnt, s)
-                cnt += 1
-        # degree stuff
-        lst = cc_model.get_node_name_lst()
-
-        dlst = g.in_degree(lst)
-        degree_listbox.delete(0, END)
-        cnt = 1
-        for d in dlst:
-            s = "N: {}, In-D: {}".format(d[0], d[1])
-            degree_listbox.insert(cnt, s)
-            cnt += 1
-        slst = cc_model.get_node_name_score_lst()
-        score_listbox.delete(0, END)
-
-        cnt = 1
-        for s in slst:
-            score_listbox.insert(cnt, s)
-            cnt += 1
-
-
-    canvas.draw()
-
-
-def reset_canvas():
-    plt.clf()
-    plt.gca().set_facecolor("grey")
-    fig.set_facecolor("black")
-
-
 def setup():
-    cc_model.reset()
-    reset_canvas()
-    g.clear()
-    n1 = cc_model.create_starter_node()
-    n2 = cc_model.create_starter_node()
-    n3 = cc_model.create_starter_node()
-    g.add_nodes_from([(n1.get_name(), n1.get_node_for_graph()),
-                      (n2.get_name(), n2.get_node_for_graph()),
-                      (n3.get_name(), n3.get_node_for_graph())])
-    g.add_edge(n1.get_name(), n2.get_name())
-    g.add_edge(n1.get_name(), n3.get_name())
-    plt.gca().set_facecolor("grey")
-    fig.set_facecolor("black")
-    draw_graph()
+    n = []
+    for i in range(0, start_node_num):
+        n.append(cc_model.create_rnd_node())
+        g.add_node(n[i].get_name())
 
+    for i in range(1, start_node_num):
+        g.add_edge(n[i-1].get_name(), n[i].get_name())
 
-setup()
-canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=True)
+    for i in range(0, start_node_num):
+        n[i].update_node_interaction(g.in_degree(n[i].get_name()), cc_model.get_count())
 
 
 def add_node_to_graph():
-    reset_canvas()
     n_new = cc_model.create_rnd_node()
+    g.add_node(n_new.get_name())
+    #print("new is: ", n_new.get_name())
     # nrp will be the parent that picks you based on similar words
-    nrp = cc_model.get_closest_match(n_new, [])
+    nrp = cc_model.get_parent_match(n_new)
+
     # nr2 and nr3 will be recs that your video provides
     # nr2 will be above average interactions and similar words
-    nr2 = cc_model.get_most_popular_match(n_new, [nrp])
+    #nr2_lst = cc_model.get_popular_matches(n_new, nrp, 1)
+
     # nr3 will be only based on similar words
-    nr3 = cc_model.get_closest_match(n_new, [nrp, nr2])
-    g.add_nodes_from([(n_new.get_name(), n_new.get_node_for_graph())])
+    nr3_lst = cc_model.get_similar_match(n_new, nrp, 3)
+
     g.add_edge(nrp.get_name(), n_new.get_name())
-    print("parent to new: {} -> {}".format(nrp.get_name(), n_new.get_name()))
-    g.add_edge(n_new.get_name(), nr2.get_name())
-    print("new to rec1: {} -> {}".format(n_new.get_name(), nr2.get_name()))
-    g.add_edge(n_new.get_name(), nr3.get_name())
-    print("new to rec2: {} -> {}".format(n_new.get_name(), nr3.get_name()))
-    n_new.update_node_interaction(g.in_degree(n_new.get_name()), cc_model.get_count())
-    nr2.update_node_interaction(g.in_degree(nr2.get_name()), cc_model.get_count())
-    nr3.update_node_interaction(g.in_degree(nr3.get_name()), cc_model.get_count())
-    # remove old edge here (don't remove n_new
+    # for n in nr2_lst:
+    #     g.add_edge(n_new.get_name(), n.get_name())
+
+    for n in nr3_lst:
+        g.add_edge(n_new.get_name(), n.get_name())
+
+    # remove node with oldest interactions
     stalest_node = cc_model.get_stale_node()
-    # get edges from node (in only)
-    if stalest_node is not None:
-        stale_node_name = stalest_node.get_name()
-        edges_in = list(g.in_edges(stale_node_name))
+    remove_edge_by_node(stalest_node)
+    edge_lst = list(g.edges)
+
+    u, v, s = cc_model.get_worse_match_nodes(edge_lst)
+    if u is not None and v is not None:
+        if (u.get_name(), v.get_name()) in edge_lst:
+            g.remove_edge(u.get_name(), v.get_name())
+            #print("1. remove an edge {}, {}, score = {}".format(u.get_name(), v.get_name(), s))
+        if (v.get_name(), u.get_name()) in edge_lst:
+            g.remove_edge(v.get_name(), u.get_name())
+            #print("2. removed an edge {}, {}, score = {}".format(v.get_name(), u.get_name(), s))
+    else:
+        pass
+        # print("didn't find a worse edge: ", edge_lst)
+
+    for n in cc_model.get_nodes():
+        n.update_node_interaction(g.in_degree(n.get_name()), cc_model.get_count())
+
+
+
+def remove_edge_by_node(node):
+    if node is not None:
+        node_name = node.get_name()
+        edges_in = list(g.in_edges(node_name))
         if len(edges_in) > 0:
-            print("edges in stalest node: ", edges_in)
             u, v = rnd.choice(edges_in)
             g.remove_edge(u, v)
-            stalest_node.update_node_interaction(g.in_degree(stale_node_name))
-            print("removing in edge from stalest node {}".format(
-                  stalest_node.get_name()))
+            node.update_node_interaction(g.in_degree(node_name), cc_model.get_count())
 
-    else:
-        print("there are no stale nodes yet")
-    # select random edge and delete
-    draw_graph()
+def go():
+    # Add first node to graph
 
+    while cc_model.get_count() <= tot_nodes:
+        add_node_to_graph()
+        node_cnt = len(g.nodes)
+        print("node cnt = ", node_cnt)
 
-root.mainloop()
+setup()
+go()
+print("skips and removals: ", cc_model.get_skips_and_removal_num())
+export_graph()
